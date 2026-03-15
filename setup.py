736ff58895c9ed8e1d5489fcaaecdc242795ab3e
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2026-03-15 12:42:39 -0600

    freeze via setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+executables = [
+    Executable("main.py", target_name="frlg_switch_seed_farmer"),
+    Executable("process_seeds.py", target_name="frlg_switch_seed_processor"),
+]
+
+setup(
+    name="FRLG Switch Seed Farmer",
+    version="1.0",
+    description="",
+    executables=executables,
+    options={
+        "build_exe": {
+            "build_exe": "dist",
+            "zip_includes": [
+                ("config.json, config.json"),
+            ],
+        }
+    },
+)
