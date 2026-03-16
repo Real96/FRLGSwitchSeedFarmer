2e811b01096bbb3a18bb581fdbe71ee6ec008d24
Real96 <chicco.gaeta@gmail.com>
2026-03-16 19:41:21 +0100

    Remove empty line

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ while seeds_counter < SEEDS_TO_COLLECT and consecutive_failures < 5:
                     prior_blink_data = blink_start_good_values[index]
                     loop_counter+=1
                     continue
-                    
+
                 prior_two = base >> 32
                 if prior_zero >= prior_two:
                     test_prior = base 
