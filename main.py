28d3eee78d340943e34721cbc71ad60fc7710555
Real96 <chicco.gaeta@gmail.com>
2026-03-16 02:41:55 +0100

    Fix spaces

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ REPEAT_TIMES = config["REPEAT_TIMES"]
 OUTPUT_FILE_NAME = config["OUTPUT_FILE_NAME"]
 DEBUG = config["DEBUG"]
 
-
 def signal_handler(_signal, _advances):  # CTRL+C handler
     print("Stop request")
     bot.close()
-
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -27,7 +25,6 @@ repeat_counter = 0
 tic = 0
 toc = 0
 
-a_press_value = A_PRESS_INITIAL_VALUE
 delay = 16.7427 / 1000 / REPEAT_TIMES
 reset_time = time()
 consecutive_failures = 0
@@ -37,7 +34,6 @@ if not os.path.exists(OUTPUT_FILE_NAME):
     with open(OUTPUT_FILE_NAME, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Seed", "Frame", "Time"])
-
 
 blink_start_good_values = [0 for _ in range(90)]
 data_zero = 0
@@ -49,13 +45,17 @@ for i in range(90):
         data_two = 30
     else:
         data_two = 60
-    data_zero+=1
+
+    data_zero += 1
+
     if data_zero > data_two:
         data_zero = 0
         data_one ^= 1
+
     blink_start_good_values[i] = (data_two << 32) | (data_one << 16) | data_zero
 
-seed_delay = INITIAL_SEED_DELAY + seed_counter
+seed_delay = INITIAL_SEED_DELAY + seeds_counter
+
 while (
     seeds_counter < SEEDS_TO_COLLECT
     and consecutive_failures < 5
@@ -63,10 +63,12 @@ while (
     # Verify the game booted and get a time stamp for an event with fixed-time relative to boot
     try:
         vblank_counter = bot.read_vblank_counter()
+
         while vblank_counter != LOW_VBLANK_HERALDING:
             if time() - reset_time > 10:
                 print("Failed to boot")
                 raise TimeoutError
+
             bot.pause(0.001)
             vblank_counter = bot.read_vblank_counter()
 
@@ -84,7 +86,6 @@ while (
         consecutive_failures += 1
         continue
 
-        
     # Stall until the BlinkPressStart task has been initialized
     try:
         while not bot.is_blink_start_initialized():
@@ -102,47 +103,60 @@ while (
         consecutive_failures += 1
         continue
 
-    # Stall until the right number of main game loops have occured 
+    # Stall until the right number of main game loops have occured
     loop_counter = 0
     prior_blink_data = 0
+
     try:
         while loop_counter < seed_delay:
             bot.pause(0.001)
             blink_data = bot.read_blink_start_counter()
-            index = loop_counter%90
+            index = loop_counter % 90
             # Data matches the next expected value in the sequence
+
             if blink_data == blink_start_good_values[index]:
-                loop_counter+=1
+                loop_counter += 1
                 prior_blink_data = blink_data
             else:
                 # Data is same as old data
                 if blink_data == prior_blink_data:
                     continue
-                prior_zero = prior_blink_data&0xFFFF
-                prior_one = (prior_blink_data>>16)&0xFFFF
-                prior_two = (prior_blink_data>>32)&0xFFFF
+
+                prior_zero = prior_blink_data & 0xFFFF
+                prior_one = (prior_blink_data >> 16) & 0xFFFF
+                prior_two = (prior_blink_data >> 32) & 0xFFFF
+
                 if prior_one == 1:
                     prior_two = 30
                 else:
                     prior_two = 60
+
                 # There are a number of edge cases that would only happen extremely rarely if we read in the middle of the function that we test for
                 test_prior = (prior_two << 32) | (prior_one << 16) | prior_zero
+
                 if prior_blink_data == test_prior:
                     continue
-                prior_zero+=1
+
+                prior_zero += 1
                 test_prior = (prior_two << 32) | (prior_one << 16) | prior_zero
+
                 if prior_blink_data == test_prior:
                     continue
+
                 if prior_zero >= prior_two:
                     prior_zero = 0
                     test_prior = (prior_two << 32) | (prior_one << 16) | prior_zero
+
                     if prior_blink_data == test_prior:
                         continue
+
                     prior_one ^= 1
                     test_prior = (prior_two << 32) | (prior_one << 16) | prior_zero
+
                     if prior_blink_data == test_prior:
                         continue
-                # None of the test cases made sense, so we raise an error because we don't understand where we are in the cycle    
+
+                # None of the test cases made sense, so we raise an error because we don't understand where we are in the cycle
                 raise ValueError(f"New data {blink_data} not consistent with old data {prior_blink_data}")
     except ValueError as e:
         print(e)
@@ -152,7 +166,7 @@ while (
         bot.restart_game(True)
         reset_time = time()
         consecutive_failures += 1
-        continue 
+        continue
     except Exception as e:
         print(
             "Error reading RAM, restarting the game and resetting the connection in 15 seconds"
@@ -167,16 +181,18 @@ while (
 
     # A press to trigger seed
     bot.press("A")
-    toc = time()            
-                    
+    toc = time()
+
 
     # Stall until seed is initialized
     ok = True
+
     try:
         while not bot.read_is_box_pointer_initialized():
             if time() - toc > 3:
                 ok = False
                 break
+
             bot.pause(0.001)
     # TODO: actual exception types
     except Exception:
@@ -190,7 +206,6 @@ while (
         reset_time = time()
         consecutive_failures += 1
         continue
-
 
     # Seed initialization timed out, reset and try again
     if not ok:
@@ -221,11 +236,12 @@ while (
 
     seeds_counter += 1
     print(
-        f"{seeds_counter:04d} - {initial_seed:04X} | {a_press_value} ({(toc - tic):.4f})"
+        f"{seeds_counter:04d} - {initial_seed:04X} | {seed_delay} ({(toc - tic):.4f})"
     )
+
     with open(OUTPUT_FILE_NAME, "a", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow([f"{initial_seed:04X}", a_press_value, toc - tic])
+        writer.writerow([f"{initial_seed:04X}", seed_delay, toc - tic])
 
     repeat_counter += 1
 
@@ -238,4 +254,3 @@ while (
     toc = 0
     bot.restart_game()
     reset_time = time()
-
