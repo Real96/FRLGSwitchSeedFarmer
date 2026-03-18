191261e126e34f731d1357ccb765fa587ba845ac
c-poole <codemeister4@gmail.com>
2026-03-18 17:12:58 -0500

    usb should be auto completed no

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 from cx_Freeze import setup, Executable
 
-IMPORT_MAP = {
-        "pyusb": "usb"
-}
-
-def get_requirements():
-    frozen_packages = []
-
-    with open("requirements.txt") as f:
-        # Filter out comments and empty lines
-        for line in f:
-            line = line.strip()
-
-            if not line or line.startswith("#"):
-                continue
-
-            module_name = IMPORT_MAP.get(line, line)
-            frozen_packages.append(module_name)
-
-        return frozen_packages
-
 executables = [
     Executable("main.py", target_name="frlg_switch_seed_farmer"),
     Executable("process_seeds.py", target_name="frlg_switch_seed_processor"),
